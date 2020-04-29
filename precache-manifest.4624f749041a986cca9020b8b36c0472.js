d1fbd4b075b3fa5edd36bfd9e8decb72cf96a9d0
Aaron Ault <aultac@gmail.com>
2020-04-29 12:06:58 -0400

    Updates

---
+++ precache-manifest.4624f749041a986cca9020b8b36c0472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dbe0526fdf6b9ea34dc29dc75517de",
+    "url": "/amounts/index.html"
+  },
+  {
+    "revision": "9f1f38d98d61821fc67d",
+    "url": "/amounts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65169637ff65e13816c1",
+    "url": "/amounts/static/js/2.641a04a6.chunk.js"
+  },
+  {
+    "revision": "cc12340550ed5874a6aa648cf8be411b",
+    "url": "/amounts/static/js/2.641a04a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1f38d98d61821fc67d",
+    "url": "/amounts/static/js/main.2f18006e.chunk.js"
+  },
+  {
+    "revision": "79049779625aa18156ab",
+    "url": "/amounts/static/js/runtime-main.25ff8ae5.js"
+  }
+]);
